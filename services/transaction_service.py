4a5da437f4386cddc75b68e21d7421d4e0226654
innurhap28 <kmugi118@gmail.com>
2026-06-19 19:49:23 +0900

    feat: TransactionService 클래스 정의 및 거래 목록, 거래 추가 기능 작성

--- services/transaction_service.py
+++ services/transaction_service.py
@@ -1,1 +1,14 @@
 # 거래 관련 기능 담당
+from storage.repository import Repository
+from typing import Iterator
+from models.transaction import Transaction
+
+class TransactionService:
+    def __init__(self):
+        self.repository = Repository()
+
+    def list_transactions(self) -> Iterator[Transaction]:
+        yield from self.repository.iter_transactions()
+
+    def add_transaction(self, transaction: Transaction) -> None:
+        self.repository.add_transaction(transaction)
